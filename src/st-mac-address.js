e47fb8458732845f0f73093e9da01399d3d89832
Maksim Beliavsky <maksimbell503@gmail.com>
2021-09-16 13:35:43 +0300

    feat: add mac solution

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+
+  let alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  for (let i = 0; i < n.length; i++) {
+    if ((i + 1) % 3 == 0 && n.charAt(i) != '-') {
+      return false;
+    } else if (alphabet.indexOf(n.charAt(i)) == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
